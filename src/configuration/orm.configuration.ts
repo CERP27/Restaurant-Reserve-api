b11f2b531c18e38298cd0a8cc77881206649c85a
ElectroCERP <carloscer27r@gmail.com>
2025-02-23 16:23:46 -0300

    Create Orm configuration file

---
+++ src/configuration/orm.configuration.ts
@@ -0,0 +1,89 @@
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+dotenv.config();
+
+export enum ENVIRONMENT {
+  PRODUCTION = 'production',
+  STAGING = 'staging',
+  DEVELOPMENT = 'development',
+  AUTOMATED_TEST = 'automated_tests',
+}
+
+const production: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  schema: process.env.DB_SCHEMA,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+const staging: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  schema: process.env.DB_SCHEMA,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+const development: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  schema: process.env.DB_SCHEMA,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+const automatedTests: DataSourceOptions = {
+  type: 'better-sqlite3',
+  database: `./data/database/tests.${Math.random()}.db`,
+  synchronize: true,
+  dropSchema: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export const datasourceOptions: DataSourceOptions = (() => {
+  if (process.env.NODE_ENV === ENVIRONMENT.PRODUCTION) {
+    return production;
+  }
+
+  if (process.env.NODE_ENV === ENVIRONMENT.STAGING) {
+    return staging;
+  }
+
+  if (process.env.NODE_ENV === ENVIRONMENT.DEVELOPMENT) {
+    return development;
+  }
+
+  if (process.env.NODE_ENV === ENVIRONMENT.AUTOMATED_TEST) {
+    return automatedTests;
+  }
+
+  throw new Error('No environment defined');
+})();
+
+export default new DataSource({
+  ...datasourceOptions,
+  entities: [
+    join(
+      __dirname,
+      '..',
+      'modules/**/infrastructure/persistence/entities/*.entity.ts',
+    ),
+  ],
+  migrations: ['./data/migrations/*.ts'],
+});
