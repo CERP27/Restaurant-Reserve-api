71482686ccdb08e5c84b8b02a3397fbcfe2b58c5
ElectroCERP <carloscer27r@gmail.com>
2025-02-23 16:21:42 -0300

    Update appModule and main.ts
    Update appModule to use GlobalPipes and GlobalInterceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { datasourceOptions } from './configuration/orm.configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { CommonModule } from './common/common.module';
+import { configurationValidate } from './configuration/configuration.validate';
+import { configuration } from './configuration/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      validationSchema: configurationValidate,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...datasourceOptions,
+        autoLoadEntities: true,
+      }),
+      dataSourceFactory: async (options) => {
+        return new DataSource(options).initialize();
+      },
+    }),
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
